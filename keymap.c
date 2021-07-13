060a17992ae927b8f1fd63769b9d5ca612171afb
derek.strasters <derek.strasters@vivint.com>
2021-07-12 23:39:59 -0600

    changed some things

--- keymap.c
+++ keymap.c
@@ -15,7 +15,8 @@ enum preonic_keycodes {
     GAME,
     QUADER,
     THREE_0,
-    TWO_0
+    TWO_0,
+    ADJUST
 };
 
 #define LCTRLSH (QK_LCTL | KC_LSFT)
@@ -23,7 +24,6 @@ enum preonic_keycodes {
 #define SYMBOL MO(_SYMBOL)
 #define NUMBERS MO(_NUMBERS)
 #define MOVE MO(_MOVE)
-#define ADJUST MO(_ADJUST)
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -37,11 +37,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [_GAME] = LAYOUT_preonic_grid(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    KC_LCTL, _______, _______, _______, KC_SPC,  KC_BTN3, _______, _______, _______, _______, _______, _______
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
+    ADJUST,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_LCTL, KC_LCTL, KC_LGUI, KC_LALT, KC_SPC,  NUMBERS, KC_SPC,  MOVE,    KC_RALT, KC_RGUI, KC_RCTL, RCTRLSH
 ),
 
 [_SYMBOL] = LAYOUT_preonic_grid(
@@ -115,6 +115,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 SEND_STRING("00");
             }
             return false;
+        case ADJUST:
+            if (record->event.pressed) {
+                layer_on(_ADJUST);
+            } else {
+                layer_off(_ADJUST);
+            }
+            return false;
+            break;
     }
 
     return true;
